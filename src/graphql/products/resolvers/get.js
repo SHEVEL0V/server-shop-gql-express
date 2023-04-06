144273136fc76d6d9b695e68f907792e6c2661a5
Андрій Шевельов <ashevelevmti@gmail.com>
2023-04-06 20:45:57 +0300

    add

--- src/graphql/products/resolvers/get.js
+++ src/graphql/products/resolvers/get.js
@@ -4,14 +4,14 @@ import { searchParams } from "../../../services/products/searchParams.js";
 import { sortParams } from "../../../services/products/sortParams.js";
 
 export default async (root, args, context) => {
-  const { limit, sort, page } = args.query || {};
+  const { limit = 4, sort, page } = args.query || {};
 
   const results = await productSchema
     //----------find product and sort and skip and limit ----------//
     .find(searchParams(args?.query))
     .sort(sortParams(sort))
     .skip(page ? limit * (page - 1) : null)
-    .limit((limit = 4));
+    .limit(limit);
 
   //--------count products--------//
   const count = await productSchema.countDocuments(searchParams(args?.query));
